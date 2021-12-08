75ea026991099da53fc8dc2d674c248dec5b416b
Adam Bujak <adamjbujak@gmail.com>
2021-12-07 19:23:36 -0500

    commit

--- ball_sim/matlab_learning.m
+++ ball_sim/matlab_learning.m
@@ -1,16 +1,7 @@
-h = 0.01;
-A = [1 h; 0 1];
-B = [0; h];
-C = [1 0];
-D = [0];
-x0 = [10; -1];
+fc = 10000;
 
-m = 1;
-g = 10;
-fa = 9.9;
-
-u(1:1000) = fa/m - g;
-t = transpose([0:0.01:9.99]);
-
-sys = ss(A, B, C, D, h);
-lsim(sys, u, t, x0)
+figure; hold on
+for N0 = 0.01:0.001:0.1
+    pe = qfunc(sqrt(5/(4*N0*fc)));
+    plot(N0, pe, '.');
+end
